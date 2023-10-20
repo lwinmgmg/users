d46ca1e0a966bd3f10b5ebc9dd35e84a781e28ab
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2023-10-21 00:03:05 +0630

    Remove unused codes

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/lwinmgmg/user/controllers"
 	"github.com/lwinmgmg/user/grpc/server"
-	"github.com/lwinmgmg/user/models"
-	"gorm.io/gorm"
 )
-
-var (
-	wg sync.WaitGroup
-)
-
-func printSomething(partner *models.Partner, db *gorm.DB) {
-	fmt.Println(partner.NextCode(db))
-	wg.Done()
-}
 
 func main() {
 	var wg sync.WaitGroup
